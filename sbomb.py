66bcc3a309e1f6e70f887da13c6e32fa18994614
Duzcu480 <semih18198@gmail.com>
2026-01-12 13:53:21 +0300

    Refactor check_service_connection function
    Refactor service connection check for specific services.

--- sbomb.py
+++ sbomb.py
@@ -16,7 +16,7 @@ for attribute in dir(SendSms):
 def check_service_connection(service_name):
     """Servis bağlantısını kontrol et (SMS göndermeden)"""
     try:
-        # Servislere özel bağlantı testleri
+        
         if service_name == "KahveDunyasi":
             url = "https://api.kahvedunyasi.com:443/api/v1/auth/account/register/phone-number"
             headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:135.0) Gecko/20100101 Firefox/135.0", "Accept": "application/json, text/plain, */*", "Accept-Encoding": "gzip, deflate, br", "Content-Type": "application/json", "X-Language-Id": "tr-TR", "X-Client-Platform": "web", "Origin": "https://www.kahvedunyasi.com", "Dnt": "1", "Sec-Gpc": "1", "Referer": "https://www.kahvedunyasi.com/", "Sec-Fetch-Dest": "empty", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Site": "same-site", "Priority": "u=0", "Te": "trailers", "Connection": "keep-alive"}
@@ -319,12 +319,11 @@ def check_service_connection(service_name):
             r = requests.get(url, timeout=3)
             return r.status_code < 500
 
-        # Diğer servisler için genel kontrol
+        
         return False
         
     except Exception as e:
         return False
-
 art = """
   ____                      _  _    ___   ___  
  |  _ \ _   _ _______ _   _| || |  ( _ ) / _ \ 
